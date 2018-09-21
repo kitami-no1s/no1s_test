ea9db8a4e39c952b991fe6ba6dfffe36015d0961
Tetsushi Kitami <tetu.k.p.227@gmail.com>
2018-09-22 00:50:20 +0900

    コメントの挿入

--- animation/walking_mario.js
+++ animation/walking_mario.js
@@ -1,3 +1,4 @@
+// 画像の配列を準備
 let images_right = [
 	"images/mario_1.png",
 	"images/mario_2.png",
@@ -9,22 +10,36 @@ let images_left = [
 	"images/mario_6.png",
 ]
 
+// マリオが走るエリア
 const container = document.querySelector('#container');
-let area = container.clientWidth;;
+let area = container.clientWidth;
+
+// マリオ
 const box = document.querySelector('#mario');
 box.style.backgroundImage = "url(" + images_right[0] +")";
+
+// 移動距離
 let y = 0;
+
+// キーコード判別用
 let keydown = '';
+
+// 画像配列操作用
 let index = 0;
 let length = 2;
 
+// ウィンドウのリサイズに対応、マリオを初期位置へ移動
 window.addEventListener('resize', e=> {
 	area = container.clientWidth;
 	box.style.left = "0px";
 });
+
+// キーボードが押されている状態
 window.addEventListener('keydown', e=> {
 	keydown = e.key;
 });
+
+// キーボードが離されたとき
 window.addEventListener('keyup', e => {
 	switch (keydown) {
 		case 'ArrowLeft':
@@ -37,9 +52,11 @@ window.addEventListener('keyup', e => {
 	keydown = '';
 });
 
+// マリオの位置を更新、画像の切り替え
 const update = () => {
 	switch (keydown) {
 		case 'ArrowLeft':
+//			エリアをはみ出さないように
 			if(y >= 5) {
 				y -= 5;
 			}
